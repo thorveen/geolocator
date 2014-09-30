1bcaf7a94045084d17c781f52841947bf366ecc6
thorveen <veen@biodiversity.ubc.ca>
2014-09-30 14:55:19 +0200

    20140927geolocator.code

---
+++ R GeoLight v1.R
@@ -0,0 +1,21 @@
+# R version 3.0.2 (2013-09-25) -- "Frisbee Sailing"
+#
+# testing BAS files in geoLight R package
+#
+#
+
+
+setwd("/Users/thorveen/ WORK/flycatchers/geolocator data/2013")
+
+list.files()
+
+data <- read.csv("1st_CL75426_000.txt", stringsAsFactors = FALSE)
+colnames(data) <- c("status", "date.time", "date.code", "light")
+str(data)
+head(data)
+
+data.t <- data[1:5000,]
+plot(data.t$light, type = "l")
+
+window <-
+
